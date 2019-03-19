df81b69b254d4c71512b7b3052cbb33daa91d035
Ricardo Brancas <anjomigel@gmail.com>
2019-03-19 14:00:53 +0000

    Does not work. Coisas

--- algos.py
+++ algos.py
@@ -1,5 +1,77 @@
+class Graph:
+    
+    def __init__(self, n):
+        self.adj = [[] for i in range(n)]
+        self.n = n
+        self.init_labels()
+
+    def __getitem__(self, key):
+        return self.adj[key]
+
+    def add_edge(self, i, j):
+        self.adj[i].append(j)
+
+    def remove_edge(self, i, j):
+        self.adj[i].remove(j)
+
+    def init_labels(self):
+        self.labels = [None] * self.n
+        self.labels[0] = 0
+
+    def get_label(self, n):
+        return self.labels[n]
+
+    def set_label(self, n, l):
+        self.labels[n] = l
+
+    def __str__(self):
+        return str(self.adj) + "\n" + str(self.labels)
+
+
+def breadth_first_search(G, s, t):
+    G.init_labels()
+    Q = [s]
+    found = False
+    while Q:
+        for n in G[Q[0]]:
+            if n == t:
+                found = True
+            if G.get_label(n) is None:
+                G.set_label(n, G.get_label(Q[0])+1)
+                Q.append(n)
+        Q = Q[1:]
+
+    return found
+
+
+def depth_first_search(G, s, t, f=None, path=[]):
+    path = path + [s]
+    if s == t:
+        if f:
+            f(path)
+    else:
+        for e in G[s]:
+            depth_first_search(G, e, t, f, path)
+
+
+def depth_first_search_1(G, s, t, f=None, path=[]):
+    path = path + [s]
+    if s == t:
+        if f:
+            f(path)
+        for i in range(len(path)-1):
+            print("(",path[i], ",", path[i+1], ")")
+            G.remove_edge(path[i], path[i+1])
+            G.add_edge(path[i+1], path[i])
+        print(G)
+
+    else:
+        for e in filter(lambda x: G.get_label(x) > G.get_label(s), G[s]):
+            depth_first_search_1(G, e, t, f, path)
+
+
 def shortest_path(G, s, t):
-    """ G should be an adjacency list:
+    """ G should be an adjacency list
         s should be the index of the source
         t should be the index of the sink
     """
@@ -39,16 +111,8 @@ def bipartite_max_flow_unweighted(G, s, t, m):
 
     pairing = [None] * m
 
-    # we don't need to compute the last shortest_path
-    p = shortest_path(G, s, t)
-    while p:
-        for u, v in p:
-            G[u].remove(v)
-            G[v].append(u)
-            if 0 < u <= m:
-                pairing[u - 1] = v - m
-
-        p = shortest_path(G, s, t)
+    while breadth_first_search(G, s, t):
+        depth_first_search_1(G, s, t, lambda x: print(x))
 
     return pairing
 
---
+++ weighted_bipartite_match.py
@@ -0,0 +1,22 @@
+#!/bin/python
+import sys
+from algos import *
+
+data = sys.stdin.readlines()
+
+m, n = map(int, data[0].split())
+g = Graph(m + n + 2)
+
+assignment = [None] * m
+
+for line in data[1:]:
+    nodes = list(map(int, line.split()))
+    g.add_edge(nodes[0], m+nodes[1])
+
+for i in range(1, m + 1):
+    g.add_edge(0, i)
+
+for i in range(1, n + 1):
+    g.add_edge(m+i, m+n+1)
+
+bipartite_max_flow_unweighted(g, 0, m+n+1, m)
